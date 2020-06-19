68c8adc66cc8c75a89c5d8057f4e7155b4e1d393
PVMI <pvmi0604@gmail.com>
2020-06-18 20:34:58 -0600

    show text api

--- app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/photogallery/PhotoGalleryFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,17 +30,17 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
     private class PhotoHolder extends  RecyclerView.ViewHolder {
-        // private TextView mTitleTextView;
+        private TextView mTitleTextView;
         private ImageView mItemImageView;
 
         public PhotoHolder(View itemView) {
             super(itemView);
-            mItemImageView = itemView.findViewById(R.id.gallery_item_imageView);
-           // mTitleTextView = (TextView) itemView;
+            //mItemImageView = itemView.findViewById(R.id.gallery_item_imageView);
+            mTitleTextView = (TextView) itemView;
         }
-        /*public void bindGalleryItem(GalleryItem item) {
+        public void bindGalleryItem(GalleryItem item) {
             mTitleTextView.setText(item.toString());
-        }*/
+        }
 
         public void bindDrawable(Drawable drawable) {
             mItemImageView.setImageDrawable(drawable);
@@ -47,35 +48,35 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
     private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
-        private List<GalleryItem> mGalleryItems;
+        private List<GalleryItem> galleryItems;
 
         public PhotoAdapter(List<GalleryItem> galleryItems) {
-            mGalleryItems = galleryItems;
+            this.galleryItems = galleryItems;
         }
 
         @NonNull
         @Override
         public PhotoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-           //TextView textView = new TextView(getActivity());
-           //return new PhotoHolder(textView);
-             LayoutInflater inflater = LayoutInflater.from(getActivity());
-            View view = inflater.inflate(R.layout.gallery_item, viewGroup, false);
-            return new PhotoHolder(view);
+           TextView textView = new TextView(getActivity());
+           return new PhotoHolder(textView);
+//             LayoutInflater inflater = LayoutInflater.from(getActivity());
+//            View view = inflater.inflate(R.layout.gallery_item, viewGroup, false);
+//            return new PhotoHolder(view);
 
         }
 
         @Override
         public void onBindViewHolder(@NonNull PhotoHolder photoHolder, int position) {
-           GalleryItem galleryItem = mGalleryItems.get(position);
-            // photoHolder.bindGalleryItem(galleryItem);
-            Drawable placeHolder = getResources().getDrawable(R.drawable.emoji);
-            photoHolder.bindDrawable(placeHolder);
+           GalleryItem galleryItem = galleryItems.get(position);
+            photoHolder.bindGalleryItem(galleryItem);
+            //Drawable placeHolder = getResources().getDrawable(R.drawable.emoji);
+            //photoHolder.bindDrawable(placeHolder);
 
         }
 
         @Override
         public int getItemCount() {
-            return mGalleryItems.size();
+            return galleryItems.size();
         }
     }
 
